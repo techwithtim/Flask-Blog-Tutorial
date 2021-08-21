984468ec750e2c21f9b605f8ff846e42e5bf078a
Jeremy Guill <rbtm2006@me.com>
2021-08-20 22:14:03 -0700

    init

---
+++ website/notion.py
@@ -0,0 +1,55 @@
+import requests, json
+
+
+def auth(auth_name):
+    with open("website/key.txt") as file:
+        data = file.read()
+        keys = json.loads(data)
+    key = keys[auth_name]
+    return key
+    
+    
+def get_supplies():
+    url = "https://api.notion.com/v1/databases/ac64bba17a8e4a3aa696543024c86206/query"
+    
+    payload = ""
+    headers = {
+    'Content-Type': 'application/json',
+    'Notion-Version': '2021-05-13',
+    'Authorization': 'Bearer ' + auth('notion')
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    jsonResponse = json.loads(response.text)
+    
+    return jsonResponse
+
+def get_menu():
+    url = "https://api.notion.com/v1/databases/79e5cfd9b9654ca492ed8290557c37da/query"
+    
+    payload = ""
+    headers = {
+    'Content-Type': 'application/json',
+    'Notion-Version': '2021-05-13',
+    'Authorization': 'Bearer ' + auth('notion')
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    jsonResponse = json.loads(response.text)
+
+    with open("menu.json",'w') as file:
+        file.write(json.dumps(jsonResponse))
+    
+    menu = []
+    for i in range(len(jsonResponse['results'])):
+        id = jsonResponse['results'][i]['id']
+        name = jsonResponse['results'][i]['properties']['Name']['title'][0]['text']['content']
+        littlemenu = {}
+        littlemenu['id'] = id
+        littlemenu['name'] = name
+        menu.append(littlemenu)
+    
+    return menu
+
+def get_meals():
+    pass
--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import User
+from .notion import get_supplies, get_menu
+from datetime import datetime
+from datetime import timedelta
 
 views = Blueprint("views", __name__)
 
@@ -14,5 +17,25 @@ def home():
 @views.route("/cpap")
 @login_required
 def cpap():
-    cpapsupplies = ['one', 'two', 'three', 'four', 'five']
+    supplies = get_supplies()
+    cpapsupplies = []
+    for i in range(len(supplies['results'])):
+        id = supplies['results'][i]['id']
+        item = supplies['results'][i]['properties']['Item']['title'][0]['plain_text']
+        itemNum = supplies['results'][i]['properties']['Item#']['rich_text'][0]['plain_text']
+        howOften = supplies['results'][i]['properties']['How often (days)']['number']
+        lastOrdered = datetime.strptime(supplies['results'][i]['properties']['Last Ordered']['date']['start'],"%Y-%m-%d")
+        imgUrl = supplies['results'][i]['properties']['ImageURL']['url']
+        nextOrder = datetime.strptime(supplies['results'][i]['properties']['Last Ordered']['date']['start'],"%Y-%m-%d") + timedelta(days=howOften)
+        littlesupply = [id,item,itemNum,howOften,lastOrdered.strftime("%m-%d-%Y"),imgUrl,nextOrder.strftime("%m-%d-%Y")]
+        cpapsupplies.append(littlesupply)
+    cpapsupplies.sort(key= lambda x: x[6])
     return render_template("cpap.html", user=User, supplies = cpapsupplies)
+
+@views.route("/menu")
+@login_required
+def menu():
+    menu = get_menu()
+    # for item in menu:
+    #         print (item['name'])
+    return render_template("menu.html", user=User, menu=menu)
